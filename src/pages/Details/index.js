37ede303e7321f0f429dfd4f653df38169dc470c
jlozovei <jlozovei@gmail.com>
2020-12-07 23:42:59 -0300

    feat: fetch species info

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -4,10 +4,35 @@ import { useParams } from 'react-router-dom';
 import { Context } from 'store';
 import { getPokemonByName, getPokemonSpecies } from 'services';
 
+import { unslugify, titleCase } from 'helpers/strings';
+import { padding } from 'helpers/numbers';
+
 const Details = () => {
   const { globalContext, setGlobalContext } = useContext(Context);
   const { current } = globalContext;
   const { name } = useParams();
+
+  function filterByLanguage({ source, language = 'en' }) {
+    const filter = source.filter((item) => {
+      return item.language.name === language;
+    });
+
+    return filter;
+  }
+
+  function getSpecies(id) {
+    getPokemonSpecies(id).then((response) => {
+      const { data } = response;
+
+      setGlobalContext({
+        ...globalContext,
+        current: {
+          ...current,
+          species_info: data
+        }
+      }).catch((err) => console.error(err));
+    });
+  }
 
   useEffect(() => {
     getPokemonByName(name)
@@ -26,10 +51,13 @@ const Details = () => {
     <div>
       <h1>Details - {name}</h1>
 
-      {current.sprites && <img src={current.sprites.front_default} alt={name} />}
+      {/* sprites from the PokeAPI are too small :( */}
+      {current.sprites && <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} alt={name} />}
 
       <div>
-        <p>Order: {current.order}</p>
+        <h2>Pokedex Data</h2>
+
+        <p>National Nº: {padding(current.id)}</p>
         {current.types && (
           <p>
             Type:{' '}
@@ -53,7 +81,7 @@ const Details = () => {
           {current.abilities &&
             current.abilities.map((item, index) => (
               <li key={index}>
-                {item.ability.name} {item.is_hidden && <span>(hidden)</span>}
+                {titleCase(unslugify(item.ability.name))} {item.is_hidden && <span>(hidden)</span>}
               </li>
             ))}
           <li></li>
@@ -68,7 +96,7 @@ const Details = () => {
               current.stats.map((item, index) => {
                 return (
                   <tr key={index}>
-                    <td>{item.stat.name}</td>
+                    <td>{titleCase(unslugify(item.stat.name))}</td>
                     <td>{item.base_stat}</td>
                   </tr>
                 );
@@ -76,6 +104,48 @@ const Details = () => {
           </tbody>
         </table>
       </div>
+
+      <button onClick={() => getSpecies(current.id)}>fetch species data</button>
+
+      {current.species_info && (
+        <React.Fragment>
+          <div>
+            <p>Base Friendship: {current.species_info.base_happiness}</p>
+            <p>Capture Rate: {current.species_info.capture_rate}</p>
+            <p>Growth Rate: {current.species_info.growth_rate.name}</p>
+            <p>Species: {filterByLanguage({ source: current.species_info.genera })[0]['genus']}</p>
+          </div>
+
+          <div>
+            <h2>Egg Groups</h2>
+            {current.species_info.egg_groups.map((item, index) => (
+              <p key={index}>{item.name}</p>
+            ))}
+          </div>
+
+          <div>
+            <h2>Names in other languages</h2>
+            <ul>
+              {current.species_info.names.map((item, index) => (
+                <li key={index}>
+                  {item.name} ({item.language.name})
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2>Pokedex Entries</h2>
+            <ul>
+              {current.species_info.pokedex_numbers.map((item, index) => (
+                <li key={index}>
+                  {titleCase(unslugify(item.pokedex.name))} - {padding(item.entry_number)}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </React.Fragment>
+      )}
     </div>
   );
 };
