d598f7cc0f79945bf9b407d32026223a11118f2c
jlozovei <jlozovei@gmail.com>
2020-12-08 17:52:06 -0300

    feat: create styled components tokens

---
+++ src/assets/styled/tokens.js
@@ -0,0 +1,27 @@
+const colors = {
+  white: 'white',
+  grey: '#c1c2c3',
+  black: 'black',
+
+  yellow: '#ffcb02',
+  gold: '#c7a005',
+  blue: '#3860ab',
+  navy: '#1d2d5e',
+  red: '#ed4125',
+  scarlet: '#8f161c'
+};
+
+const fonts = {
+  title: '',
+  text: '',
+  mono: ''
+};
+
+const viewports = {
+  mobile: '492px',
+  tablet: '768px',
+  desktop: '992px',
+  fullhd: '1280px'
+};
+
+export { colors, fonts, viewports };
