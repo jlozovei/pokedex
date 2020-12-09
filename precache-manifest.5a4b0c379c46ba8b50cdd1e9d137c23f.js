65e65e0d18631ecd932fbebc1c207b08ca0ff604
jlozovei <jlozovei@users.noreply.github.com>
2020-12-09 21:12:12 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.5a4b0c379c46ba8b50cdd1e9d137c23f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7246ff48738703359a100131c781cd9f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "928267a4d527237b0989",
+    "url": "./static/css/main.6f22d07e.chunk.css"
+  },
+  {
+    "revision": "d5fdb40fda7bb24d785c",
+    "url": "./static/js/2.7ebcf653.chunk.js"
+  },
+  {
+    "revision": "928267a4d527237b0989",
+    "url": "./static/js/main.9ce7964c.chunk.js"
+  },
+  {
+    "revision": "a1bd66594e52c1684f4b",
+    "url": "./static/js/runtime-main.7220129d.js"
+  },
+  {
+    "revision": "464910fd71d42f5603cf2b8677548432",
+    "url": "./static/media/ash.464910fd.jpg"
+  },
+  {
+    "revision": "0a2770ace20edb4992ce783668753439",
+    "url": "./static/media/jlozovei.0a2770ac.svg"
+  },
+  {
+    "revision": "6374dbdd5018a18b3732b9126b872c18",
+    "url": "./static/media/pokemon-logo.6374dbdd.png"
+  }
+]);
