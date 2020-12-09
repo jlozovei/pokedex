df8a51649bb6e10a2b00f13a078c7dc6aa45baf5
jlozovei <jlozovei@gmail.com>
2020-12-09 17:36:16 -0300

    feat: use router basename only on production

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,18 +6,24 @@ import { About } from 'pages/About';
 import { Details } from 'pages/Details';
 import { Page404 } from 'pages/404';
 
-const Routes = () => (
-  const basename = '/pokedex';
+const Routes = () => {
+  const isLocal =
+    window.location.hostname.includes('localhost') ||
+    window.location.hostname.includes('127.0.0.1');
 
-  <Router basename={basename}>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/pokemon/:pokemonId" component={Details} />
+  const basename = isLocal ? '' : '/pokedex';
 
-      <Route path="*" component={Page404} />
-    </Switch>
-  </Router>
-);
+  return (
+    <Router basename={basename}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/pokemon/:pokemonId" component={Details} />
+
+        <Route path="*" component={Page404} />
+      </Switch>
+    </Router>
+  );
+};
 
 export { Routes };
