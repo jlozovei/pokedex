3c8783056d4f28751c2af787da9b87c6327bb05c
jlozovei <jlozovei@gmail.com>
2020-12-09 22:28:33 -0300

    feat: add Generation services

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 export { PokeAPI } from './config';
 
 export { getPokemons, getPokemonByName, getPokemonSpecies } from './Pokemon';
+export { getGenerations, getGenerationByName } from './Generation';
